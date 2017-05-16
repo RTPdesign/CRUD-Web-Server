2e1dc5f782ceb5da43aa318c090d2d08ac9c4bf6
Ryan <ryan@rtpdesign.com>
2017-05-16 17:40:24 -0600

    Updated Routes

--- CRUDWebServer/build/routes/index.js
+++ CRUDWebServer/build/routes/index.js
@@ -6,9 +6,6 @@
 // import {updateTask} from '../services/userService';
 // import {removeTask} from '../services/userService';
 
-'use strict';
-
-
 import {removeTask, updateTask, findTask, createTask} from '../services/taskService';
 
 export default (app) => {
